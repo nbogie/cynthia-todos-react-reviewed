761b4e23bd247cc116697459b266d2f135a70009
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-21 17:19:58 +0100

    rendered list of todos

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,3 +1,30 @@
-function ListTodo() {
-  return <></>;
+import { useEffect, useState, Fragment } from "react";
+import getErrorMessage from "../utils/getErrorMessage";
+import axios, { all } from "axios";
+import { apiBaseURL } from "../utils/apiBaseURL";
+import TodoType from "./TodoTaskType";
+
+export default function ListTodo() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const getTodos = async () => {
+    try {
+      const response = await axios.get(apiBaseURL + "/todos");
+      const allTodoData = response.data;
+      setTodos(allTodoData);
+    } catch (error) {
+      console.error(getErrorMessage(error));
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const listAllTodos = todos.map((eachTodo) => (
+    <li key={eachTodo.todo_id}> {eachTodo.description}</li>
+  ));
+  return (
+    <Fragment>
+      <ul>{listAllTodos}</ul>
+    </Fragment>
+  );
 }
--- src/components/MainTodo.tsx
+++ src/components/MainTodo.tsx
@@ -1,10 +1,12 @@
 import InputTodo from "./InputTodo";
+import ListTodo from "./ListTodo";
 
 export default function MainTodo() {
   return (
     <>
       <h1>Get Things Done</h1>
       <InputTodo />
+      <ListTodo />
     </>
   );
 }
