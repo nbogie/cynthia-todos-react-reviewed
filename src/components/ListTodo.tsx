1578af4a499dae1fe0d5aa6926599b0eabbb07c9
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-21 20:59:36 +0100

    converting the TodoDB properties to match TodoItem

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,15 +1,31 @@
 import { useEffect, useState, Fragment } from "react";
 import getErrorMessage from "../utils/getErrorMessage";
-import axios, { all } from "axios";
+import axios from "axios";
 import { apiBaseURL } from "../utils/apiBaseURL";
-import TodoType from "./TodoTaskType";
+import { TodoDB, TodoItem } from "./TodoTaskType";
+import { LuEdit } from "react-icons/lu";
+import { MdOutlineDelete } from "react-icons/md";
 
 export default function ListTodo() {
-  const [todos, setTodos] = useState<TodoType[]>([]);
+  const [todos, setTodos] = useState<TodoItem[]>([]);
   const getTodos = async () => {
     try {
       const response = await axios.get(apiBaseURL + "/todos");
-      const allTodoData = response.data;
+      const allTodoData = response.data.map((todo: TodoDB) => {
+        const {
+          todo_id: todoId,
+          description,
+          creation_date: creationDate,
+          completed,
+        } = todo;
+        return {
+          todoId,
+          description,
+          creationDate,
+          completed,
+        };
+      });
+      console.log(allTodoData);
       setTodos(allTodoData);
     } catch (error) {
       console.error(getErrorMessage(error));
@@ -19,12 +35,35 @@ export default function ListTodo() {
     getTodos();
   }, []);
 
+  const handleDelete = async (todoId: number) => {
+    try {
+      const deleteTodo = await axios.delete(`${apiBaseURL}/todos/${todoId}}`);
+      setTodos(todos.filter((todo) => todo.todoId !== todoId));
+    } catch (error) {
+      console.error(getErrorMessage(error));
+    }
+  };
+
   const listAllTodos = todos.map((eachTodo) => (
-    <li key={eachTodo.todo_id}> {eachTodo.description}</li>
+    <Fragment key={eachTodo.todoId}>
+      <li>
+        {eachTodo.description}
+        <button>
+          <LuEdit />
+        </button>
+        <button
+          onClick={() => {
+            handleDelete(eachTodo.todoId as number);
+          }}
+        >
+          <MdOutlineDelete />
+        </button>
+      </li>
+    </Fragment>
   ));
   return (
-    <Fragment>
+    <>
       <ul>{listAllTodos}</ul>
-    </Fragment>
+    </>
   );
 }
