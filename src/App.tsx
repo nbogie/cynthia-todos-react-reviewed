aae0f99252f1d2afb2098d37b74f0fa2d0197da4
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-24 14:44:00 +0100

    moved content from MainTodo to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import MainTodo from "./components/MainTodo";
-function App(): JSX.Element {
+import { Heading } from "@chakra-ui/react";
+import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
+import { Footer } from "./components/Footer";
+
+export default function App(): JSX.Element {
   return (
     <>
-      <MainTodo />
+      <Heading as="h1" size="3xl" noOfLines={1}>
+        Get Things Done
+      </Heading>
+      <InputTodo />
+      <ListTodo />
+      <Footer />
     </>
   );
 }
-
-export default App;
