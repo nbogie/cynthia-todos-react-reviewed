78a0ace81b415287086ad9dfed634cd9d24e06eb
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-24 15:22:50 +0100

    adding Box and Flex component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import InputTodo from "./components/InputTodo";
 import TodoList from "./components/TodoList";
 import { Footer } from "./components/Footer";
@@ -6,12 +6,16 @@ import { Footer } from "./components/Footer";
 export default function App(): JSX.Element {
   return (
     <>
-      <Heading as="h1" size="3xl" noOfLines={1}>
-        Get Things Done
-      </Heading>
-      <InputTodo />
-      <TodoList />
-      <Footer />
+      <Box>
+        <Flex align="center" h="100%" justify="flex-start" direction="column">
+          <Heading as="h1" size="3xl" noOfLines={1}>
+            Get Things Done
+          </Heading>
+          <InputTodo />
+          <TodoList />
+          <Footer />
+        </Flex>
+      </Box>
     </>
   );
 }
